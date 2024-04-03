ed672c9a108b2195654c2cfad928644554a86f7c
AzureYakubu <obsmediaofficial@gmail.com>
2024-04-03 02:23:55 +0000

    added interface for TranslationService

---
+++ Leet Translator/Services/Interfaces/ITranslationService.cs
@@ -0,0 +1,10 @@
+﻿using Leet_Translator.Models;
+
+namespace Leet_Translator.Services.Interfaces
+{
+    public interface ITranslationService
+    {
+        Task<string> TranslateToLeetSpeak(string inputText);
+        IEnumerable<TranslationRecord> GetTranslationRecords();
+    }
+}

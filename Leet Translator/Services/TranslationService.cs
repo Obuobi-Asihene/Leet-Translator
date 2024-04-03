71704ad8d3719159dad2f2558e4214b7d3e1b575
AzureYakubu <obsmediaofficial@gmail.com>
2024-04-03 02:25:11 +0000

    added TranslationService for interacting with DbContext and Funtranslation service

---
+++ Leet Translator/Services/TranslationService.cs
@@ -0,0 +1,41 @@
+﻿using Leet_Translator.Data;
+using Leet_Translator.Models;
+using Leet_Translator.Services.Interfaces;
+
+namespace Leet_Translator.Services
+{
+    public class TranslationService : ITranslationService
+    {
+        private readonly IFunTranslationService _funTranslationService;
+        private readonly LeetTranslatorDbContext _dbContext;
+
+        public TranslationService(IFunTranslationService funTranslationService, LeetTranslatorDbContext dbContext)
+        {
+            _funTranslationService = funTranslationService;
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<TranslationRecord> GetTranslationRecords()
+        {
+            return _dbContext.TranslationRecords.ToList();
+        }
+
+        public async Task<string> TranslateToLeetSpeak(string inputText)
+        {
+            string translatedText = await _funTranslationService.TranslateToLeetSpeak(inputText);
+
+            //logging API call & result to DB
+            var translationRecord = new TranslationRecord
+            {
+                InputText = inputText,
+                TranslatedText = translatedText,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            _dbContext.TranslationRecords.Add(translationRecord);
+            await _dbContext.SaveChangesAsync();
+
+            return translatedText;
+        }
+    }
+}

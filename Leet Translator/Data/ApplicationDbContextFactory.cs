ea0db21d8362bf77e13ad9459667c1e900ba7110
AzureYakubu <obsmediaofficial@gmail.com>
2024-04-03 23:20:15 +0000

    added dbcontext for migration

---
+++ Leet Translator/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Leet_Translator.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<LeetTranslatorDbContext>
+    {
+        public LeetTranslatorDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LeetTranslatorDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EcommerceDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new LeetTranslatorDbContext(optionsBuilder.Options);
+        }
+    }
+}

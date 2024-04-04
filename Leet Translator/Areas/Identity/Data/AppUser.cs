bb0604a3cf955b89ff52a66639019e061af2a5e5
AzureYakubu <obsmediaofficial@gmail.com>
2024-04-04 23:07:17 +0000

    added additional properties

--- Leet Translator/Areas/Identity/Data/AppUser.cs
+++ Leet Translator/Areas/Identity/Data/AppUser.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace Leet_Translator.Areas.Identity.Data;
@@ -9,5 +6,12 @@ namespace Leet_Translator.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the AppUser class
 public class AppUser : IdentityUser
 {
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; }
 }
 

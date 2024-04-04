d5f687a51a0941fff9847d361976be1843ce51e2
AzureYakubu <obsmediaofficial@gmail.com>
2024-04-04 01:16:03 +0000

    added logrecord model

---
+++ Leet Translator/Models/LogRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace Leet_Translator.Models
+{
+    public class LogRecord
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Level { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string InputText { get; set; }
+        public string TranslatedText { get; set; }
+    }
+}
